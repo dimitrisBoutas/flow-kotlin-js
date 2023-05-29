ef7e5e1958640fd0bcf88f472fc3536f5c7a7a25
Dimitris Boutas <dimitris.boutas@nowmax.be>
2023-05-29 13:12:17 +0200

    npm publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     kotlin("multiplatform") version "1.8.21"
     id("dev.petuska.npm.publish") version "3.3.1"
@@ -24,17 +23,31 @@ kotlin {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.7.1")
                 implementation("org.jetbrains.kotlin-wrappers:kotlin-js:1.0.0-pre.550")
-
             }
         }
     }
 
 
     npmPublish {
+
+        packages {
+            named("js") {
+                packageJson {
+                    author {
+                        name.set("Dimitris Boutas")
+                    }
+                    repository {
+                        type.set("git")
+                        url.set("https://github.com/dimitrisBoutas/flow-kotlin-js.git")
+                    }
+                }
+            }
+        }
+
         registries {
-            register("npmjs") {
-                uri.set("https://registry.npmjs.org")
-                authToken.set("obfuscated")
+            github {
+                dry.set(false)
+                authToken.set(System.getenv("GITHUB_TOKEN"))
             }
         }
     }
